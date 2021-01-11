f2cc9d40506a492e0f136115406c4d43a7dc8ab3
I-Rinka <I_Rinka@outlook.com>
2021-01-11 14:47:19 +0800

    initial

---
+++ DisplayImage.cpp
@@ -0,0 +1,22 @@
+#include <opencv4/opencv2/opencv.hpp>
+using namespace cv;
+int main(int argc, char const *argv[])
+{
+    if (argc < 2)
+    {
+        return -1;
+    }
+    Mat image;
+    image = imread(argv[1], 1);
+
+    if (!image.data)
+    {
+        return -1;
+    }
+    namedWindow("Dispay Image", WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+
+    waitKey(0);
+
+    return 0;
+}
---
+++ Learning.md
@@ -0,0 +1,21 @@
+# OpenCV大作业
+
+## OpenCV使用
+
+## Git使用
+
+### 初始化
+
+```shell
+git init # 初始化本地仓库
+git add * # 跟踪
+git rm --cached CMakeFiles -r # 取消跟踪某一文件夹
+```
+
+### 查看
+
+`git status -s`
+
+## Cmake使用
+
+## GDB使用
