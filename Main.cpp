cae2c17b42069f824781ebd5bbfece80156eb541
I-Rinka <I_Rinka@outlook.com>
2021-01-12 20:52:24 +0800

    at least, it runs

--- Main.cpp
+++ Main.cpp
@@ -42,14 +42,34 @@ public:
 
     //获得一条线的斜率和截距的参数
     void GetOneLine(int &k_return, int &b_return);
+
+    //输入参数在原图画线
+    static void PrintLineToImage(Mat &input, int p, int theta);
 };
+
+void Hough_Fantasy::PrintLineToImage(Mat &input, int p, int theta)
+{
+    int max_row = input.rows;
+    double cost = cos(((double)theta / 180.0) * M_PI), sint = sin(((double)theta / 180.0) * M_PI);
+    for (int i = 0; i < max_row; i++)
+    {
+        int y = (p - i * cost) / sint;
+        uchar *p = GetPoint(input, i, y);
+        if (p != NULL)
+        {
+            *p = 255;
+        }
+    }
+}
+
 void Hough_Fantasy::GetOnePoint(int &p_return, int &theta_return)
 {
     unsigned int max = 0;
     int rt_theta = -1, rt_p = -1;
+    int pmin = -(this->max_p), pmax = this->max_p;
     for (int theta = -90; theta <= 180; theta++)
     {
-        for (int p = -this->max_p; p < this->max_p; p++)
+        for (int p = pmin; p < pmax; p++)
         {
             unsigned int *point = GetHoughPoint(theta, p);
             if (point != NULL)
@@ -71,7 +91,7 @@ unsigned int *Hough_Fantasy::GetHoughPoint(int theta, int p)
 {
     //因为用的是偏移的，所以最后返回霍夫坐标的时候也要记得减去偏移量!
     //p有可能是送进来负数的。这里还可以再加一个边界值判断
-    return this->hough_mat_pointer + ((90 + 180) * theta + p + max_p);
+    return this->hough_mat_pointer + ((90 + 180) * (theta + 90) + p + max_p);
 }
 
 void Hough_Fantasy::DrawInHoughMat(int x, int y)
@@ -80,7 +100,7 @@ void Hough_Fantasy::DrawInHoughMat(int x, int y)
     for (int i = -90; i <= 180; i++)
     {
         //P=xcosθ+ysinθ
-        int P = x * cos(i) + y * sin(i);
+        int P = x * cos(((double)i / 180.0) * M_PI) + y * sin(((double)i / 180.0) * M_PI);
         unsigned int *point = GetHoughPoint(i, P);
         if (point != NULL)
         {
@@ -94,7 +114,6 @@ void Hough_Fantasy::DoHoughTransition()
 {
     int rows = this->input_mat.rows;
     int cols = this->input_mat.cols;
-    this->max_p = (int)(sqrt(rows * rows + cols * cols) + 0.5);
 
     //长是θ，而宽是两倍的最大截距
     this->hough_mat_pointer = (unsigned int *)calloc(this->max_p * 2 * (90 + 180), sizeof(unsigned int));
@@ -125,8 +144,12 @@ void Hough_Fantasy::DoHoughTransition()
 //阈值，目前只支持输入灰度图像
 Hough_Fantasy::Hough_Fantasy(Mat &input, int threshold)
 {
-    this->threshold = threshold;
     this->input_mat = input;
+    this->threshold = threshold;
+
+        int rows = this->input_mat.rows;
+    int cols = this->input_mat.cols;
+    this->max_p = (int)(sqrt(rows * rows + cols * cols) + 0.5);
 }
 
 Hough_Fantasy::~Hough_Fantasy()
@@ -136,6 +159,24 @@ Hough_Fantasy::~Hough_Fantasy()
 
 int main(int argc, char const *argv[])
 {
+    Mat image = imread("line.png", 1);
 
+    cvtColor(image, image, COLOR_RGB2GRAY);
+
+    namedWindow("Display", WINDOW_AUTOSIZE);
+    imshow("Display", image);
+    // waitKey(0);
+
+    Hough_Fantasy my_hough(image, 50);
+
+    my_hough.DoHoughTransition();
+
+    int p = 0, theta = 0;
+    my_hough.GetOnePoint(p, theta);
+
+    my_hough.PrintLineToImage(image, p, theta);
+    namedWindow("Display2", WINDOW_AUTOSIZE);
+    imshow("Display2", image);
+    waitKey(0);
     return 0;
 }
